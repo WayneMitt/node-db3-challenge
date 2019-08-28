27f22c78fa68f2c1400caa71407e9df174b42c65
Wayne Mitt <wmitt85@gmail.com>
2019-08-27 19:17:39 -0500

    added order by step_number on findSteps and finished remaining three helper methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,9 +6,9 @@ module.exports = {
     find,
     findById,
     findSteps,
-    // add,
-    // update,
-    // remove
+    add,
+    update,
+    remove
 };
 
 function find() {
@@ -18,9 +18,28 @@ function find() {
 function findById(id) {
     return db('schemes')
         .where({ id })
+        .first()
 }
 
 function findSteps(id) {
     return db('steps')
         .where({ scheme_id: id })
+        .orderBy('step_number')
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+}
+
+function update(scheme, id) {
+    return db('schemes')
+        .where({ id })
+        .update(scheme)
+}
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del()
 }
